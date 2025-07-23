116817d5459c4c3b1cdea6b600fe30dca2409f79
Michał Sacewicz <gatorek@gmail.com>
2025-07-23 15:09:31 +0200

    aloow to assing to a variable

--- lib/assigner.ex
+++ lib/assigner.ex
@@ -7,8 +7,24 @@ defmodule Assigner do
   end
 
   defmacro assign(value, var) do
+    case var do
+      {name, meta, context} when is_atom(name) and is_list(meta) and is_atom(context) ->
+        assign_by_variable(value, var)
+
+      var when is_atom(var) ->
+        assign_by_atom(value, var)
+    end
+  end
+
+  defp assign_by_atom(value, var) do
     quote do
       var!(unquote({var, [], Elixir})) = unquote(value)
+    end
+  end
+
+  defp assign_by_variable(value, var) do
+    quote do
+      var!(unquote(var)) = unquote(value)
     end
   end
 end
--- test/assigner_test.exs
+++ test/assigner_test.exs
@@ -6,10 +6,25 @@ defmodule AssignerTest do
 
   test "assing a value to a variable by atom" do
     assign(1, :a)
+
     assert a == 1
 
     2 + 2
-    |> assign(:b)
+    |> assign(b) # no need to initialize a variable
+
     assert b == 4
+  end
+
+  test "assigning a value to a variable by variable" do
+    # no need to initialize a variable
+    assign(2, a)
+
+    assert a == 2
+
+    1..3
+    |> Enum.map(& &1 + 1)
+    |> assign(b)
+
+    assert b == [2, 3, 4]
   end
 end
